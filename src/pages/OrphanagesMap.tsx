8167a257b572339bf70a02a44a6e8b9ccda6d5cf
João Paulo de Proença <joao18paulo91@gmail.com>
2020-10-14 15:42:08 -0300

    Primeira aula

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -2,9 +2,13 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { FiPlus } from 'react-icons/fi';
 
+import { Map, TileLayer } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+
 import mapMarkerImg from '../images/map-marker.svg';
 
 import '../styles/pages/orphanages-map.css';
+
 
 function OrphanagesMap() {
     return (
@@ -23,7 +27,16 @@ function OrphanagesMap() {
                 </footer>
             </aside>
 
-            <div></div>
+            <Map
+                center={[-23.5192395,-47.4196327]}
+                zoom={15}
+                style={{ width: '100%', height: '100%' }}
+            >
+                <TileLayer url="http://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+                {/* <TileLayer
+                 url={'https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}'} 
+                 /> */}
+            </Map>
 
             <Link to="" className="create-orphanage">
                 <FiPlus size={32} color="#fff" />
